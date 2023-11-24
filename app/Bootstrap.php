baa2592a74b1c6e987bb6f3d8b4d0bc03b35b9a6
Petr Gürth <girtas35@gmail.com>
2023-11-24 10:11:55 +0100

    feat: disable tracy

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -14,7 +14,7 @@ class Bootstrap
 		$configurator = new Configurator;
 		$appDir = dirname(__DIR__);
 
-		$configurator->setDebugMode(true);
+		$configurator->setDebugMode(false);
 		$configurator->enableTracy($appDir . '/log');
 
 		$configurator->setTimeZone('Europe/Prague');
